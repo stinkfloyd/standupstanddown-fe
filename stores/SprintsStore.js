19eb7cd381a612dc09a1a9da74300e686140b9bc
Tori Mitchell <tori.zoe.mitchell@gmail.com>
2018-12-12 08:52:02 -0700

    added stores folder

---
+++ stores/SprintsStore.js
@@ -0,0 +1,21 @@
+const SprintsStore = {
+
+  data: {
+    sprints: [],
+  },
+
+  methods: {
+
+    //this data will be present when the el is mounted. gets the team's sprints and puts it in the sprints property.
+    mounted() {
+      fetch("")
+        .then(response => response.json())
+        .then((data) => {
+          this.usersTeams = data;
+        })
+    },
+
+  }
+};
+
+export default SprintsStore;
