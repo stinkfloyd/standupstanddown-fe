b9f278a70cb9d0ba2dc553361603ec80f90cb6cc
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-12-14 20:04:22 -0700

    "Couple fixes to for that add sprint modal"

--- src/stores/SprintStore.js
+++ src/stores/SprintStore.js
@@ -9,44 +9,42 @@ const SprintStore = {
   methods: {
 
     async getSprint(id, name) {
-     return fetch(`http://localhost:3000/sprints/${id}`, {
+      return fetch(`http://localhost:3000/sprints/${id}`, {
         credentials: 'include',
         method: "GET",
         headers: {
           "Content-Type": "application/json",
           Accept: "application/json",
         },
-      }).then(async(response) => {
+      }).then(async (response) => {
         console.log("response: ", response)
-          let resJson = await response.json()
-          console.log("resJson: ", resJson)
-          console.log("resJson[0]: ", resJson[0])
-          SprintStore.data.sprintInfo.push(resJson[0])
-          SprintStore.data.teamName = name
-          console.log("SprintStore.data.sprintInfo: ", SprintStore.data.sprintInfo)
-          console.log("resJson: ", resJson)
-          return resJson
-
+        let resJson = await response.json()
+        console.log("resJson: ", resJson)
+        console.log("resJson[0]: ", resJson[0])
+        SprintStore.data.sprintInfo.push(resJson[0])
+        SprintStore.data.teamName = name
+        console.log("SprintStore.data.sprintInfo: ", SprintStore.data.sprintInfo)
+        console.log("resJson: ", resJson)
+        return resJson
       })
-
     },
 
     async postSprint(team_id, sprint_length, sprint_goal) {
       console.log("in the sprintstore postSprint rte:", team_id, sprint_length, sprint_goal)
-      let postBody = { sprint_length, sprint_goal}
+      let postBody = { sprint_length, sprint_goal }
       await fetch(`http://localhost:3000/sprints/${team_id}`, {
         credentials: 'include',
         method: "POST",
-        body: postBody,
+        body: JSON.stringify(postBody),
         headers: {
           "Content-Type": "application/json",
           Accept: "application/json",
         },
       })
-      // getting an error back for 400 CORS header...
-      .then(async (response) => {
-        console.log(response)
-        }) 
+        // getting an error back for 400 CORS header...
+        .then(async (response) => {
+          console.log(response)
+        })
     },
 
   }
